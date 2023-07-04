43ff56d00a3ffbbdb2c05819af7c16f603e85789
satyam <satyam.adg@gmail.com>
2023-07-04 20:05:24 +0530

    b2 1st commit

--- file1.js
+++ file1.js
@@ -13,3 +13,9 @@ console.log("b1 branch file1 line 11");
 console.log("b1 branch file1 line 13");
 
 console.log("b1 branch file1 line 14");
+
+console.log("b2 branch file1 line 17");
+
+console.log("b2 branch file1 line 19");
+
+console.log("b2 branch file1 line 21");
